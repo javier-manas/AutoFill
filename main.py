d44846a70d5c1fefc5f42397350f8ebdff912977
javier-manas <javiermbuf@gmail.com>
2026-03-05 22:20:12 +0100

    Fix comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import keyboard
 import time
 import random
 
+#Powershell:
 # & "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="C:\chrome_debug_profile"
 # https://sauce-demo.myshopify.com/account/register
 
